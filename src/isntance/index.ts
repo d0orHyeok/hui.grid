5e949382cdcea77dd7bc33c1689b20a6a87d5080
JangHyeok Kim <d0or.hyeok@gmail.com>
2023-12-22 10:13:10 +0900

    feat: accessKey option

--- src/isntance/index.ts
+++ src/isntance/index.ts
@@ -8,6 +8,7 @@ import { create as createRowCoords } from '@/isntance/rowCoords';
 import { create as createViewport } from '@/isntance/viewport';
 import { create as createColumnCoords } from '@/isntance/columnCoords';
 import { generateId } from '@/utils/common';
+import { find$ } from '@/utils/dom';
 
 export default function createInstance(opts: Observable<OptGrid>): Instance {
   const root = `hui-${generateId()}`;
@@ -23,6 +24,14 @@ export default function createInstance(opts: Observable<OptGrid>): Instance {
   const rowCoords = createRowCoords({ demension, source, viewport });
   // Create ColumnCoords
   const columnCoords = createColumnCoords({ column, viewport });
+
+  let prevAccessKey: string | undefined = undefined;
+  opts.subscribe(({ accessKey }) => {
+    if (accessKey === prevAccessKey) return;
+    prevAccessKey = accessKey;
+    const $el = find$('.' + root);
+    if ($el) !accessKey ? $el.removeAttribute('accesskey') : ($el.accessKey = accessKey);
+  });
 
   return { root, column, columnCoords, demension, rowCoords, source, viewport };
 }
